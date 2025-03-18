fb8afffa18222456462e815ea1f2c6c23e0bbcdf
PerchunPak <git@perchun.it>
2025-03-18 19:05:55 +0100

    Add Timer component

---
+++ src/pages/Timer.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { normalizeProps, useMachine } from "@zag-js/svelte";
+  import * as timer from "@zag-js/timer";
+
+  const id = $props.id();
+  const service = useMachine(timer.machine, {
+    id,
+    countdown: true,
+    autoStart: true,
+    startMs: timer.parse({ days: 2, seconds: 10 }),
+    onComplete() {
+      console.log("Timer completed");
+    },
+  });
+  const api = $derived(timer.connect(service, normalizeProps));
+</script>
+
+<div
+  {...api.getRootProps()}
+  class="inline-flex items-center gap-1 text-4xl sm:text-6xl md:text-8xl"
+>
+  <div {...api.getItemProps({ type: "days" })}>
+    {api.formattedTime.days}
+  </div>
+  <div {...api.getSeparatorProps()}>:</div>
+  <div {...api.getItemProps({ type: "hours" })}>
+    {api.formattedTime.hours}
+  </div>
+  <div {...api.getSeparatorProps()}>:</div>
+  <div {...api.getItemProps({ type: "minutes" })}>
+    {api.formattedTime.minutes}
+  </div>
+  <div {...api.getSeparatorProps()}>:</div>
+  <div {...api.getItemProps({ type: "seconds" })}>
+    {api.formattedTime.seconds}
+  </div>
+</div>
