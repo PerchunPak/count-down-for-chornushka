0659c59466d95a2251ec645835bb33e444bdedb8
PerchunPak <git@perchun.it>
2025-04-08 19:25:44 +0200

    Update date

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -26,7 +26,7 @@
 
   onMount(() => {
     service = createService({
-      targetDate: new Date("2025-04-07T11:00:00.000Z"),
+      targetDate: new Date("2025-05-01T11:00:00.000Z"),
       now: new Date(),
     });
   });
